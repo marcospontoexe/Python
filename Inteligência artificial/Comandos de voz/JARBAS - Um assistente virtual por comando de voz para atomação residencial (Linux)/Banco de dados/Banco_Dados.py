f3446d7362a12f0ff6d20bb441b81a4119efaa12
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-06-21 19:33:37 -0300

    a

--- Inteligência artificial/Comandos de voz/JARBAS - Um assistente virtual por comando de voz para atomação residencial (Linux)/Banco de dados/Banco_Dados.py
+++ Inteligência artificial/Comandos de voz/JARBAS - Um assistente virtual por comando de voz para atomação residencial (Linux)/Banco de dados/Banco_Dados.py
@@ -6,9 +6,9 @@ comandos = ["JARBAS", "LIGUE", "DESLIGUE", "MÚSICA", "SALA", "QUARTO", "COZINHA
 repeticoes = 3
 
 for i in range(0, len(comandos)):
-    for rodada in range (0, repeticoes): # grava o o comando t r s vezes
-        print(f"\nDIGA {comandos[i]} ...\n")
-        gravar = "arecord-D hw: audiocodec, 0 -f dat -d 2 -c 1 -r 16000"
-        arecord -d 10 -r 48000 -c 2 -f S16_LE audio.wav
+    for rodada in range (0, repeticoes):    #grava os comandos têrs vezes
+        print(f"\nDIGA {comandos[i]}...\n")
+        gravar = "arecord -D hw: audiocodec, 0 -f dat -d 2 -c 1 -r 16000"
+        #arecord -d 10 -r 48000 -c 2 -f S16_LE audio.wav
         cmd = str(gravar) + str(speaker) + str("-") + str(i) + str ("-") + str(rodada) + str(".wav")
-        os. system (cmd)
+        os.system(cmd)
