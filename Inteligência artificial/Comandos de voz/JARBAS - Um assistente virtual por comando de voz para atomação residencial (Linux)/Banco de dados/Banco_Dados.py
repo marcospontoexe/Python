7416e9078589a676ed1e563df8ba3e932593542a
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2021-06-23 16:35:17 -0300

    código Banco de dados
    esse código captura audio de dois segundos para cada comando a ser gravado

--- Inteligência artificial/Comandos de voz/JARBAS - Um assistente virtual por comando de voz para atomação residencial (Linux)/Banco de dados/Banco_Dados.py
+++ Inteligência artificial/Comandos de voz/JARBAS - Um assistente virtual por comando de voz para atomação residencial (Linux)/Banco de dados/Banco_Dados.py
@@ -8,11 +8,4 @@ repeticoes = 3
 for i in range(0, len(comandos)):
     for rodada in range (0, repeticoes):    #grava os comandos têrs vezes
         print(f"\nDIGA {comandos[i]}...\n")
-        
-        #arecord -d 10 -r 48000 -c 2 -f S16_LE audio.wav
-        #gravar = "arecord -D hw:audiocodec, 0 -f dat -d 2 -c 1 -r 16000"
-        #cmd = str(gravar) + str(speaker) + str("-") + str(i) + str ("-") + str(rodada) + str(".wav")
-        
-        #cmd = 'arecord -D hw:audiocodec,0 -f dat -d 2 -c 1 -r 16000 ' + str(speaker) + str('-') + str(i) + str('-') + str(rodada) + str('.wav')
-        #os.system(cmd)
-        os.system(f'arecord -D hw:0,0 -f dat -d 2 -c 1 -r 16000 {speaker}-{i}-{rodada}.wav')
+        os.system(f"arecord -D hw:0,0 -f dat -d 2 -c 2 -r 16000 {speaker}-{comandos[i]}-{rodada}.wav")
