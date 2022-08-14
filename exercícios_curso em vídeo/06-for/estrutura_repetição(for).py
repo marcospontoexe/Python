01b605b92bee11586df8f7c202d1b62b7afa70bd
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-08-14 11:31:25 -0300

    Update estrutura_repetição(for).py

--- exercícios_curso em vídeo/06-for/estrutura_repetição(for).py
+++ exercícios_curso em vídeo/06-for/estrutura_repetição(for).py
@@ -1,40 +1,39 @@
 '''
-01 - MOstre na tela uma contagem regressiva de 10 até 0, com intervalo de um segundo;
-02 - Mostre todos os números pares num intervalo de 0 a 50;
-03 - Calcule a soma entre todos os numeros ímpares, que são multiplos de trÊs e que se encontram
+01 - Mostre a tabuada de um número escolhido pelo usuário:
+02 - MOstre na tela uma contagem regressiva de 10 até 0, com intervalo de um segundo;
+03 - Mostre todos os números pares num intervalo de 0 a 50;
+04 - Calcule a soma entre todos os numeros ímpares, que são multiplos de trÊs e que se encontram
 num intervalo de 1 até 500
-04 - Mostre a tabuada de um número escolhido pelo usuário:
 05- Leia uma frase qualquer e diga se é um palíndromo:
 06 - Leia o peso de cinco pessoas e diga qual é o maior e o menor peso:
 '''
 #=============Ex01=============
-from time import sleep
-print('{:=^30}' .format('Ex01'))
-for i in range (10, -1, -1):
-    print(i, end=" ")
-    sleep(1)
-
-#=============Ex02=============
-print('{:=^30}' .format('Ex02'))
-for i in range (0, 51, 2):
-    print(i, end=" ")
-
-#=============Ex03=============
-print('{:=^30}' .format('Ex03'))
-soma = int(0)
-for i in range (1, 501, 2): #o incremento em dois em dois, só incrementa os números ímpares
-    if i%3 == 0:
-        soma += i
-print("A soma dos números é {}!" .format(soma))
-
-#=============Ex04=============
 print('{:=^30}' .format('Ex04'))
 tabuada = int(input('Digite a tabuada desejada: '))
-for i in range (0, 11):
+for i in range (0, 11):                                 #repete de 0 até 10, incrementando de 1 em 1
     print("{} X {} = {}" .format(tabuada, i, tabuada*i))
 
 print('O \033[35mTiago\033[m da o boga!!!')
 
+#=============Ex02=============
+from time import sleep
+print('{:=^30}' .format('Ex01'))
+for i in range (10, -1, -1):        #repete de 10 até 0, decrementando de um em um
+    print(i, end=" ")
+    sleep(1)
+
+#=============Ex03=============
+print('{:=^30}' .format('Ex02'))
+for i in range (0, 51, 2):          ##repete de 0 até 50, incrementando de 2 em 2
+    print(i, end=" ")
+
+#=============Ex04=============
+print('{:=^30}' .format('Ex03'))
+soma = int(0)
+for i in range (1, 501, 2): #o incremento em dois em dois, só incrementa os números ímpares
+    if i%3 == 0:            #se for multiplo de 3
+        soma += i
+print("A soma dos números é {}!" .format(soma))
 
 #=============Ex05=============
 print('{:=^30}' .format('Ex05'))
