bbe87b2d45db3767cca79f187c6ec34df2e6c20b
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2022-08-17 15:08:49 -0300

    Update tratamento_de_erro.py

--- exercícios_curso em vídeo/tratamento de erros/tratamento_de_erro.py
+++ exercícios_curso em vídeo/tratamento de erros/tratamento_de_erro.py
@@ -1,22 +1,34 @@
+'''
+try:(tenta executar o código.)
+    OPERAÇÃO;
 
+except: (caso aconteça algum erro (falha sintática) ou exceção (erro de semântica) no bloco try, é executado o bloco except)
+    FALHOU;
 
+else: (caso não retorne nehuma exceção ou erro no bloco try, é executado o bloco else)
+    DEU CERTO;
+
+finally: (o bloco sempre é executado)
+    FALHOU ou DEU CERTO;
+
+'''
 
 try:
     a = int(input("Digite um inteiro: "))
     b = int(input("Digite outro inteiro: "))
-    r = a/b
+    r = a / b
 
-except (ValueError, TypeError):
-    print("Tivemos um problema com o tipo de trat_erro digitado!")
+except (ValueError, TypeError):  # bloco executado quando as exceções 'ValueError ou TypeError' é gerada no bloco try
+    print("Tivemos um problema com os valores digitado!")
 
-except (ZeroDivisionError):
+except (ZeroDivisionError):  # bloco executado quando a exceção 'ZeroDivisionError' é gerada no bloco try
     print("Este programa não consegue dividor por zero!")
 
-except KeyboardInterrupt:
+except KeyboardInterrupt:  # bloco executado quando a exceção 'KeyboardInterrupt' é gerada no bloco try
     print("O usuário preferiu não informar os dados!")
 
 
-except Exception as erro:
+except Exception as erro:  # mostra qual foi o erro retornado pela exceção
     print(f"A classe do erro encontrado foi {erro.__class__}!")
     print(f"O erro encontrado foi {erro.__cause__}!")
 
@@ -25,3 +37,4 @@ else:
 
 finally:
     print("Volte sempre.")
+
