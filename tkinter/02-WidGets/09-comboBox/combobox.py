1347aaaca3df9e8646ac647dfec5c7e2b7e95c16
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2023-01-22 12:44:01 -0300

    .

--- tkinter/02-WidGets/09-comboBox/combobox.py
+++ tkinter/02-WidGets/09-comboBox/combobox.py
@@ -1,6 +1,9 @@
 from tkinter import *
 from tkinter import ttk    # sub biblioteca para usar o combo box
 
+def escolha():
+    print(cbEsp.get())
+
 janela = Tk()  # instancia um objeto da classe Tk()
 
 # ----------CONFIGURAÇÕES DA JANELA-------------------
@@ -17,5 +20,8 @@ lbEsp.pack()
 cbEsp = ttk.Combobox(janela, values=listEsporte)        # cria o combo box
 cbEsp.set("skate")                                      #define o valor skate como sendo o padrão do combo box
 cbEsp.pack()
+
+bt = Button(janela, text="escolher", command=escolha)
+bt.pack()
 
 janela.mainloop()  # cria um laço de repetição enquanto a janela estiver aberta
