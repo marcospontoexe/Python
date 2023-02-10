b3e3a035786987989743870c04190b624af89980
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2023-02-09 21:16:32 -0300

    Update bind.py

--- tkinter/03-eventos/01-bind/bind.py
+++ tkinter/03-eventos/01-bind/bind.py
@@ -1,17 +1,17 @@
 from tkinter import *
-from tkinter import ttk
+#from tkinter import ttk
 
 
 janela = Tk()  # instancia um objeto da classe Tk()
-
-def botaoEsquerdo(retorno):
-    print(retorno)
 
 # ----------CONFIGURAÇÕES DA JANELA-------------------
 janela.title("Janela principal")  # título da janela
 janela["bg"] = "grey"  # altera o valor do índice do dicionário "janela"
 janela.geometry("500x300+200+100")  # define o posicionamento e tamanho da tela (largura X altura + distância à esqueda + dist topo) em pixel
 # ----------------------------------------------------
+
+def botaoEsquerdo(retorno):
+    print(f"x={retorno.x} | y={retorno.y} | geometria={janela.geometry()}")
 
 janela.bind("<Button-1>", botaoEsquerdo)       # evento para botão esquerdo do mouse
 
