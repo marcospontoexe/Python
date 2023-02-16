0195c6b256a5421966826023d89b14493360d916
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2023-02-16 00:12:31 -0300

    coletando preços

--- MT5/analista quant/01-conectando/main.py
+++ MT5/analista quant/01-conectando/main.py
@@ -3,8 +3,12 @@ import MetaTrader5 as mt5       #importa a biblioteca
 mt5.initialize()        #inicia o metatrader 5
 
 d = mt5.terminal_info()
-
+#print(type(d))
 print(f"informações do terminal: {d}")
 
+d = d._asdict()         #transforma a classe em dicionário
+
+for k in d.keys():
+    print(f"{k} -> {d[k]}")
 
 mt5.shutdown()      #encerra o metatrader
--- MT5/analista quant/02-adquirindo preço/main.py
+++ MT5/analista quant/02-adquirindo preço/main.py
@@ -1,13 +1,30 @@
 import MetaTrader5 as mt5       #importa a biblioteca
+from datetime import datetime
+import pandas as pd
 
 mt5.initialize()        #inicia o metatrader 5
 
+'''
 d = mt5.terminal_info()     #a classe 'd' recebe as informações do mt5
 
 d = d._asdict()         #transforma a classe em dicionário
 
 for k in d.keys():
     print(f"{k} -> {d[k]}")
+'''
 
+#----------COLETANDO PREÇO-------------
+ativo = "WINJ23"        # nome do ativo a ser pesquisado
+flag= mt5.COPY_TICKS_ALL   # flag para selecionar
+dia = datetime(2022,12,14)      # data inicial da pesquisa
+
+dados = mt5.copy_ticks_from(ativo, dia, 10, flag)       # a Array "dados" recebe as informações do "ativo"
+
+arq = pd.DataFrame(dados, columns=["time","bid","ask","last","volume","time_msc","volume_real"])
+print(arq)
+
+print(type(arq))
 
 mt5.shutdown()      #encerra o metatrader
+
+
