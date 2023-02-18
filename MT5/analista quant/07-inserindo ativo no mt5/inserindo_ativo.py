bcb793c6d0e9db4f67b07ce010574be8f32f8a56
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2023-02-18 14:45:23 -0300

    inserindo um ativo no meta trader 5

---
+++ MT5/analista quant/07-inserindo ativo no mt5/inserindo_ativo.py
@@ -0,0 +1,13 @@
+import MetaTrader5 as mt5       #importa a biblioteca
+import pandas as pd
+
+if not mt5.initialize():        #inicia o metatrader 5
+    print(f"Não foi possível iniciar o MT5! -> {mt5.last_error()}")
+    quit()
+
+#----------inserindo o ativo no meta trader 5-------------
+ativo = "WINM23"        # nome do ativo a ser pesquisado
+# é importante o ativo estar aberto na plataforma para retornar valores (compra, venda, volume ...) corretos
+mt5.symbol_select(ativo, True)        # insere o ativo no meta trader 5
+
+mt5.shutdown()      #encerra o metatrader
