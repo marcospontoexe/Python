fe3ba1c67bc47afbb095d2f73e88b7a21e1f6f26
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2023-02-19 20:01:07 -0300

    lendo csv

--- MT5/analista quant/09-lendo_csv/importando_csv.py
+++ MT5/analista quant/09-lendo_csv/importando_csv.py
@@ -2,3 +2,8 @@ import time
 import MetaTrader5 as mt5       #importa a biblioteca
 from datetime import datetime
 import pandas as pd
+
+
+d = pd.read_csv("WIN$_H1.csv", sep="\t")        # lendo um arquivo csv com separação por tabulação
+print(d)
+print(d.shape)      # mostra o total de colunas e linhas
