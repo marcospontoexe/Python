d8e08a83f56ddf972a1c1417a939c8309399b598
marcos <marcos.daniel1990@hotmail.com>
2023-02-21 22:33:41 -0300

    abrindo CSV

---
+++ Pandas/01-abrir_arquivos/open.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+#----------------ABRINDO ARQUIVO CSV-----------------------------
+arquivo = "dadosCSV.csv"   # nome do arquivo
+codificacao = "UTF-8"   # tipo de codificaçao do arquivo
+separador = ","         # separador usado na separação  dos dados CSV
+cabecalho = 0           # define qual linha será o cabeçalho
+selecionarCol = ["time", "open", "high", "low", "close", "real_volume"] # seleciona quais colunas o dataframe deve possuir
+numLinhas = 50      # seleciona quantas linhas o dataframe deve possuir
+dadosCSV = pd.read_csv(arquivo, encoding=codificacao, sep=separador, header=cabecalho, usecols=selecionarCol, nrows=numLinhas)        # abre um arquivo CSV
+
+print(dadosCSV)
