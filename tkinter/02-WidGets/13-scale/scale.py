be2cfff888e018063bcdb0e5a323cb12f11115e7
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2023-03-15 22:20:41 -0300

    scale
    tk interface

--- tkinter/02-WidGets/13-scale/scale.py
+++ tkinter/02-WidGets/13-scale/scale.py
@@ -2,6 +2,9 @@ from tkinter import *
 
 def escolha():
     print(f"Valor da escala: {sc.get()}")
+def escolha1(val):
+    print(f"Valor da escala: {sc.get()}")
+
 
 janela = Tk()  # instancia um objeto da classe Tk()
 
@@ -11,8 +14,9 @@ janela["bg"] = "grey"  # altera o valor do índice do dicionário "janela"
 janela.geometry("500x300+200+100")  # define o posicionamento e tamanho da tela (largura X altura + distância à esqueda + dist topo) em pixel
 # ----------------------------------------------------
 
-sc = Scale(janela, from_=0, to=100, orient=HORIZONTAL)
+sc = Scale(janela, from_=0, to=100, orient=HORIZONTAL, resolution=10, command=escolha1)
 sc.set(50)
+
 sc.pack()
 
 bt = Button(janela, text="Escolher", command=escolha)
