b1d62179c267225071dd25752e0a24ee315b7676
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2023-04-09 01:00:21 -0300

    .

--- exercícios_curso em vídeo/01-variáveis/variaveis.py
+++ exercícios_curso em vídeo/01-variáveis/variaveis.py
@@ -1,16 +1,11 @@
-nome = input('digite seu nome: ')
+'''#-----String-----------------------
+nome = input('digite seu nome: ')       #recebe uma string
 idade = input('digite sua idade: ')
 peso = input('digite seu peso: ')
 
+
 print('seu nome é {}, vc tem {} anos e pesa {} quilos!'.format(nome,idade,peso)) #impressão formatada (string), usada no python 3.0
-
 print(f'seu nome é {nome}, vc tem {idade} anos e pesa {peso} quilos!') #impressão formatada  (string), usada no python 3.6
-
-n1 = int(input('Digite um número: '))       #toda variável de entrada é do tipo string. o int() converte para int
-print(type(n1))                             #mostra o tipo da variávell
-n2 = int(input('Digite outro número: '))
-res = n1+n2
-print('A soma entre {} e {} vale {}!'.format(n1,n2,res))
 
 print('oi'+'ola')
 print('oi'*5)
@@ -20,8 +15,18 @@ print('olá {:20}.'.format(valor))
 print('olá {:>20}.'.format(valor))      #Alinha a string à direita, e completa a string com 20 caracteres nulo
 print('olá {:^20}.'.format(valor))         #Deixa a string com 20 caracteres, completando com caracteres nulos à esquerda e direita
 print('olá {:=^21}.'.format(valor))     #Deixa a string com 21 caracteres, completando com '=' à esquerda e direita
+print('continuação da \nlinha anterior')
+#-----------------------------------
 
+#---------int e float----------------------------------
+n1 = int(input('Digite um número int: '))       #toda variável de entrada é do tipo string. o int() converte para int
+print(type(n1))                             #mostra o tipo da variávell
+n2 = float(input('Digite outro número float: '))    #vonverte string para float
+res = n1+n2
+print('A soma entre {} e {} vale {}!'.format(n1,n2,res))
+'''
 numero = 5.65987445525
 print(f'{numero:.3f}', end='-')     #formata o float com três números após a vírgula. O "end='-'" concatena o print posterior com o anterior
-print('continuação da \nlinha anterior')
-
+n = round(numero,3)                 # arredonda o float para o valor mais proximo. O parâmetro à direita é a qntd de casas decimais
+print(f' Número arredondado: {n}')
+#----------------------------------------------------
