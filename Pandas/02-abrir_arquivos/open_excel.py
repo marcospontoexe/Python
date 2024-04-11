418d26e67df82b427b6a792cf209b5c8a3acb421
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2024-04-11 10:39:31 -0300

    Update open_excel.py

--- Pandas/02-abrir_arquivos/open_excel.py
+++ Pandas/02-abrir_arquivos/open_excel.py
@@ -1,14 +1,17 @@
 import pandas as pd
+import os
 
 # Para abrir excel é necessário instalar a biblioteca openpyxl
-#----------------ABRINDO ARQUIVO EXCEL-----------------------------
-arquivo = "dadosEXCEL.xlsx"   # nome do arquivo
-pagina = 1      # escolhe a página do excel. (tb pode usar o nome da página como índice)
-dados = pd.read_excel(arquivo, sheet_name=pagina)        # abre um arquivo EXCEL
+# ----------------ABRINDO ARQUIVO EXCEL-----------------------------
+diretorio_script = os.path.dirname(os.path.realpath(__file__))
+arquivo = f"{diretorio_script}/dadosEXCEL.xlsx"   # nome do arquivo
+# escolhe a página do excel. (tb pode usar o nome da página como índice)
+pagina = 1
+# abre um arquivo EXCEL
+dados = pd.read_excel(arquivo, sheet_name=pagina)
 
 print(dados)
 
-#---------VERIFICANDO O NOME DAS PÁGINAS DO EXCEL-----------------
+# ---------VERIFICANDO O NOME DAS PÁGINAS DO EXCEL-----------------
 arq = pd.ExcelFile(arquivo)     # abre um arquivo em excel
 print(arq.sheet_names)     # retorna uma lista com os nomes das páginas do excel
-
