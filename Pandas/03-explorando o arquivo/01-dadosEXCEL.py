2b85434a4d74ea0bdba2124a42d9fb984ee642ce
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2024-04-11 13:40:56 -0300

    .

--- Pandas/03-explorando o arquivo/01-dadosEXCEL.py
+++ Pandas/03-explorando o arquivo/01-dadosEXCEL.py
@@ -1,36 +1,50 @@
 import pandas as pd
+import os
+'''
+# ----------------ABRINDO ARQUIVO EXCEL-----------------------------
+diretorio_script = os.path.dirname(os.path.realpath(__file__))
+arquivo = f"{diretorio_script}/dadosEXCEL.xlsx"   # nome do arquivo
 
-#----------------ABRINDO ARQUIVO EXCEL-----------------------------
-arquivo = "dadosEXCEL.xlsx"   # nome do arquivo
-pagina = 0      # escolhe a página do excel. (tb pode usar o nome da página como índice)
-dados = pd.read_excel(arquivo, sheet_name=pagina)        # abre um arquivo EXCEL
+# escolhe a página do excel. (tb pode usar o nome da página como índice)
+pagina = 0
+# abre um arquivo EXCEL
+dados = pd.read_excel(arquivo, sheet_name=pagina)
 
-print(dados[["open", "close"]]) # mostra apenas as colunas selecionadas
+print(dados[["open", "close"]])  # mostra apenas as colunas selecionadas
 print(dados["open"][3])     # mostra o valor da coluna X linha
 print('----------------------------------------------------------')
+'''
+diretorio_script = os.path.dirname(os.path.realpath(__file__))
+arquivo = f"{diretorio_script}/nomes.xlsx"   # nome do arquivo
 
-arquivo = "nomes.xlsx"   # nome do arquivo
 arq = pd.read_excel(arquivo)        # abre um arquivo EXCEL
-qntd = arq["FirstName"].value_counts()        #retorna a quantidades de linhas com o mesmo "FirstName"
+'''
+# retorna a quantidades de linhas com o mesmo "FirstName"
+qntd = arq["FirstName"].value_counts()
 print(f"quantidade de nomes iguais: \n{qntd}")
-print(f"unique(): \n{arq['FirstName'].unique()}")   #mostra os valores da coluna, sem repetir o mesmo nome
+# mostra os valores da coluna, sem repetir o mesmo nome
+print(f"unique(): \n{arq['FirstName'].unique()}")
 print('----------------------------------------------------------')
 
 filtro = arq.FirstName == "John"   # recebe uma série contendo, "True" quando os valores da coluna "FirstName" é "John", e "False" caso contrário
 print(f"soma: {sum(filtro * 1)}")    # mostra quantas linhas que contém o valor "John"
 print(arq.FirstName[filtro])         # mostra apenas as linhas que contém o valor "John"
 print('------------------------------------------------------')
-
-print(f"linhas vazias: \n{(arq[arq['NameStyle'].isnull()])}")  #mostra os valores do objeto "dados" em que os índices da coluna "Tipo_Anuncio" estão vazios
+'''
+print(f"linhas vazias: \n{(arq[arq['NameStyle'].isnull(
+)])}")  # mostra os valores do objeto "dados" em que os índices da coluna "Tipo_Anuncio" estão vazios
 print('------------------------------------------------------')
 
-#---------RENOMEANDO VALORES----------------
+# ---------RENOMEANDO VALORES----------------
 print("--------arq------")
 print(arq.info())
 print("-----arqRenomeado------")
-arqRenomeado = arq.rename(columns={"FirstName":"Primeiro Nome" , "LastName":"Sobrenome"}) # "arqRenomeado" recebe um dataframe com as aletrações solicitadas
+# "arqRenomeado" recebe um dataframe com as aletrações solicitadas
+arqRenomeado = arq.rename(
+    columns={"FirstName": "Primeiro Nome", "LastName": "Sobrenome"})
 print(arqRenomeado.info())
 print("--------arq após 'inplace'------")
-arq.rename(columns={"FirstName":"Primeiro Nome" , "LastName":"Sobrenome"}, inplace=True)    # faz a alteração no próprio dataframe
+# faz a alteração no próprio dataframe
+arq.rename(columns={"FirstName": "Primeiro Nome",
+           "LastName": "Sobrenome"}, inplace=True)
 print(arq.info())
-
--- Pandas/03-explorando o arquivo/05-limpando_dataframe.py
+++ Pandas/03-explorando o arquivo/05-limpando_dataframe.py
@@ -1,37 +1,47 @@
 import pandas as pd
+import os
 
-arq = pd.read_csv("prices.csv", sep=";")        # abre um arquivo EXCEL
+diretorio_script = os.path.dirname(os.path.realpath(__file__))
+arquivo = f"{diretorio_script}/prices.csv"      # nome do arquivo
+arq = pd.read_csv(arquivo, sep=";")        # abre um arquivo EXCEL
 
 arqCopia = arq.copy()       # copia uma dataframe para outro
 
 # Convertendo dados do tipo 'objeto' para 'datatime'
 arqCopia["DATA INICIAL"] = pd.to_datetime(arqCopia["DATA INICIAL"])
 arqCopia["DATA FINAL"] = pd.to_datetime(arqCopia["DATA FINAL"])
-#------------------------------------------------------------
+# ------------------------------------------------------------
 
+print(arqCopia.info())
 # convertendo objetos para tipo numérico---------------------
-lista = ["PREÇO MÉDIO DISTRIBUIÇÃO", "DESVIO PADRÃO DISTRIBUIÇÃO", "PREÇO MÍNIMO DISTRIBUIÇÃO", "PREÇO MÁXIMO DISTRIBUIÇÃO", "COEF DE VARIAÇÃO DISTRIBUIÇÃO"]
+lista = ["PREÇO MÉDIO DISTRIBUIÇÃO", "DESVIO PADRÃO DISTRIBUIÇÃO",
+         "PREÇO MÍNIMO DISTRIBUIÇÃO", "PREÇO MÁXIMO DISTRIBUIÇÃO", "COEF DE VARIAÇÃO DISTRIBUIÇÃO"]
 for atributo in lista:
     arqCopia[atributo] = pd.to_numeric(arqCopia[atributo], errors="coerce")
+
+print(arqCopia.info())
 '''
 errors: {‘ignore’, ‘raise’, ‘coerce’}, default ‘raise’
     *raise -> Conversão inválida inválida gerará uma exceção.
     *coerce -> Conversão inválida será definida como NaN.
     *ignore -> não realiza a conversão.
 '''
-#------------------------------------------------------------
+# ------------------------------------------------------------
 
-#----Verificando quais valores são NaN-----------------------
-eh_NaN = arqCopia["PREÇO MÉDIO DISTRIBUIÇÃO"].isnull()  # retorna "True" caso o valor seja NaN
+# ----Verificando quais valores são NaN-----------------------
+# retorna "True" caso o valor seja NaN
+eh_NaN = arqCopia["PREÇO MÉDIO DISTRIBUIÇÃO"].isnull()
 print('Linhas que não foram convertidas de "object" para "numeric":\n')
-print(arq[eh_NaN]["PREÇO MÉDIO DISTRIBUIÇÃO"])        # mostra no dataframe original quais linhas não foram convertidas de 'object' para 'numeric' e seu valor
+# mostra no dataframe original quais linhas não foram convertidas de 'object' para 'numeric' e seu valor
+print(arq[eh_NaN]["PREÇO MÉDIO DISTRIBUIÇÃO"])
 print('----------------------------------------------------')
 print('Valores que não foram convertidas de "object" para "numeric":\n')
-print(arq[eh_NaN]["PREÇO MÉDIO DISTRIBUIÇÃO"].unique())        # mostra no dataframe original quais valores não foram convertidas de 'object' para 'numeric'
+# mostra no dataframe original quais valores não foram convertidas de 'object' para 'numeric'
+print(arq[eh_NaN]["PREÇO MÉDIO DISTRIBUIÇÃO"].unique())
 print('----------------------------------------------------')
-#------------------------------------------------------------
+# ------------------------------------------------------------
 
-#-----removendo linhas do dataframe que possuem valor NaN
+# -----removendo linhas do dataframe que possuem valor NaN
 arqCopia.dropna(inplace=True)
 
 print(arqCopia.info())
