c98632c6c2b438278445850e35f819d84834dfbe
marcos daniel santana <Marcos.Daniel1990@hotmail.com>
2024-04-11 13:48:11 -0300

    Update filter.py

--- Pandas/05-filtrando valores/filter.py
+++ Pandas/05-filtrando valores/filter.py
@@ -1,11 +1,15 @@
 import pandas as pd
 
-#----------------ABRINDO ARQUIVO EXCEL-----------------------------
+# ----------------ABRINDO ARQUIVO EXCEL-----------------------------
 arquivo = "dadosEXCEL.xlsx"   # nome do arquivo
-pagina = 0      # escolhe a página do excel. (tb pode usar o nome da página como índice)
-dados = pd.read_excel(arquivo, sheet_name=pagina)        # abre um arquivo EXCEL
+# escolhe a página do excel. (tb pode usar o nome da página como índice)
+pagina = 0
+# abre um arquivo EXCEL
+dados = pd.read_excel(arquivo, sheet_name=pagina)
 
 # o filter permite realizar busca mais avançada (é possível usar o regex)
-print(dados.filter(items=["open", "close"])) # mostra as colunas solicitadas
-print(dados.filter(like="o"))       # mostra todas as colunas que contem a letra 'o' no cabeçalho
-print(dados.filter(regex=".os."))       # mostra todas as colunas que contem "qualquer letra" + "o" + "qualquer letra" no cabeçalho
+print(dados.filter(items=["open", "close"]))  # mostra as colunas solicitadas
+# mostra todas as colunas que contem a letra 'o' no cabeçalho
+print(dados.filter(like="o"))
+# mostra todas as colunas que contem "qualquer letra" + "os" + "qualquer letra" no cabeçalho
+print(dados.filter(regex=".os."))
